2a773667dc02a1d5d3e91c99a31fa200fe7d3fe5
Laurent Valle <laurent.valle@outlook.com>
2020-08-04 20:11:31 -0400

    mofification: thank.html, favicon, database.csv

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -10,18 +11,17 @@ def home():
 def back_home(page_name):
     return render_template(page_name)
 
-def write_to_txt_file(data):
-    with open('database.txt', mode='a') as file:
-        name = data['name']
-        email = data['email']
-        message = data['message']
-        file.write(f'\n{name} {email} {message}')
+def write_to_csv_file(data):
+    with open('database.csv', newline='', mode='a') as file:
+        fieldnames = ['name','email','message']
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writerow(data)
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_txt_file(data)
+        write_to_csv_file(data)
         return redirect('/thank.html')
     else:
         return 'error while requesting data'
